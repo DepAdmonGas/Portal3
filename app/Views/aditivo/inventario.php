2c205559cefd000be47866c9da4daa0d6ac82cb3
GestoGas <gestogas.e@gmail.com>
2026-02-06 12:07:59 -0600

    Bitacora Aditivo

---
+++ app/Views/aditivo/inventario.php
@@ -0,0 +1,15 @@
+
+  <div class="action-btn layout-top-spacing d-flex align-items-center justify-content-between flex-wrap">
+    <h1 class="mb-0 fs-7"><?= $title ?></h1>
+  </div>
+
+  
+
+<nav class="" aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">
+            Library
+        </li>
+    </ol>
+</nav>
---
+++ public/assets/js/bitacora/aditivo.datatable.init.js
@@ -0,0 +1,92 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    $('#table-aditivo').DataTable({
+        processing: true,
+        serverSide: false,
+        autoWidth: false,
+        stateSave: true, 
+        order: [[0, 'desc']],
+        language: {
+            url: '/assets/libs/datatables.net/js/es-ES.json'
+        },
+        ajax: {
+            url: '/bitacora-aditivo/datatable',
+            type: 'GET',
+            dataSrc: function (json) {
+                return json.data;
+            }
+        },
+        columns: [
+            { data: 'id', width: '60px', className: 'text-center' },
+            { data: 'folio' },
+            { data: 'fecha',
+                render: function (data, type) {
+                if (!data) return '';
+
+                 if (type !== 'display') {
+                    return data;
+                }
+
+                const fecha = new Date(data);
+
+                return fecha.toLocaleDateString('es-MX', {
+                    day: 'numeric',
+                    month: 'long',
+                    year: 'numeric'
+                });
+            }
+            },
+            { data: 'litros' },
+            
+            { data: 'no_factura' },
+            { data: 'producto' },
+            { data: 'galones' },
+            { data: 'inventario_fisico' },
+            {
+                data: 'estado',
+                width: '80px',
+                className: 'text-center',
+                render: function (data) {
+                    return data == 1
+                        ? '<span class="mb-1 badge text-bg-success">Activo</span>'
+                        : '<span class="mb-1 badge text-bg-danger">Eliminado</span>';
+                }
+            },
+            {
+                data: null,
+                width: '1%',
+                orderable: false,
+                searchable: false,
+                className: 'text-center align-middle td-small',
+                render: function (data, type, row) {
+                    const disabled = row.estatus === 0
+                        ? 'disabled opacity-50 pointer-events-none'
+                        : '';
+
+                    return `
+                        <div class="dropdown dropstart">
+                            <a href="javascript:void(0)" class="text-muted" data-bs-toggle="dropdown">
+                                <i class="ti ti-dots-vertical fs-6"></i>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a class="dropdown-item d-flex align-items-center gap-3 btn-edit ${disabled}" 
+                                    data-id="${row.id}" 
+                                    data-nombre="${row.nombre}">
+                                        <i class="fs-4 ti ti-edit"></i>Editar
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item d-flex align-items-center gap-3 btn-delete ${disabled}" data-id="${row.id}">
+                                        <i class="fs-4 ti ti-trash"></i>Eliminar
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    `;
+                }
+            }
+        ]
+    });
+
+});
