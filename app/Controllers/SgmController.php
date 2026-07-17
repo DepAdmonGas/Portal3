a4977f1f57a3b5e807a8dd641f924863637a6c38
DepAdmonGas <dep.sistemas@admongas.com.mx>
2026-07-17 17:41:45 -0600

    8. Gestión de Riesgos que impactan en la medición

--- app/Controllers/SgmController.php
+++ app/Controllers/SgmController.php
@@ -3,8 +3,11 @@ namespace App\Controllers;
 use App\Core\View;
 use App\Models\Sgm\Elemento;
 use App\Core\Breadcrumb;
+use App\Services\ModuloService;
 
-class SgmController{
+class SgmController extends BaseController{
+
+protected string $modulo = 'sgm';
 
     public function index(){
 
@@ -34,9 +37,6 @@ class SgmController{
 
     //------------------------------------------------------------------------------------
     //------------------------------------------------------------------------------------
-
-
-
 
     public function gestionRecursos(){
 
@@ -89,13 +89,25 @@ class SgmController{
         Breadcrumb::add('SGM', '/sgm');
         Breadcrumb::add($title, '');
 
+        $permisos = ModuloService::permisosSesion($this->modulo);
+
          $data = [
-            'title' => $title,
+             'title' => $title,
+            'permisos' => $permisos,
+            'modulo' => $this->modulo,
+            'filtro_usuario' => $this->filtro_usuario,
              'links' =>[
-                
+               '/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css',
+                '/libs/select2/dist/css/select2.min.css'
             ],
             'scripts' => [
-                '/assets/js/vendor.min.js'
+                 '/js/vendor.min.js',
+                '/libs/datatables.net/js/jquery.dataTables.min.js',
+                '/libs/select2/dist/js/select2.full.min.js',
+                '/libs/select2/dist/js/select2.min.js',
+
+                '/js/asistencia/listaasistencia.actions.init.js?v=1.0.2',
+                '/js/asistencia/listaasistencia.datatable.init.js?v=1.0.2',
             ],
             'help' => true
         ];
